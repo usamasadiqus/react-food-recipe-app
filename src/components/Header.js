c411a459a32fe2ae8edf20cc548c6f815f8433d6
Usama Sadiq <usamasadiq029@gmail.com>
2021-02-11 14:28:20 +0500

    Some changes in src and start header with jumbotron

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="jumbotron text-center">
+      <h1 className="display-1">
+        <span class="material-icons brand-icon">fastfood</span> Food Recipe
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
