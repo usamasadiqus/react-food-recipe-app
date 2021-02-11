a807cd9b2d8bdf9d7a325c768632a002751e7a70
Usama Sadiq <usamasadiq029@gmail.com>
2021-02-11 14:30:48 +0500

    Add input group(input field with button) in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,16 @@ const Header = () => {
   return (
     <div className="jumbotron text-center">
       <h1 className="display-1">
-        <span class="material-icons brand-icon">fastfood</span> Food Recipe
+        <span className="material-icons brand-icon">fastfood</span> Food Recipe
       </h1>
+      <div className="input-group w-50 mx-auto">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search Your Recipe..."
+        />
+        <button className="btn btn-dark">Search Recipe</button>
+      </div>
     </div>
   );
 };
